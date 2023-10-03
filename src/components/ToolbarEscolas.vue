fc1630781fd842f255ec778bae2ce8bb2304b1a8
Tiago Razera <tiago.razera@pelservicos.com.br>
2023-10-03 09:03:25 -0300

    toolbar com evento de clique no titulo

--- src/components/ToolbarEscolas.vue
+++ src/components/ToolbarEscolas.vue
@@ -13,7 +13,7 @@
           <ion-icon slot="icon-only" :icon="chevronBack"/>
         </ion-button>
       </ion-buttons>
-      <ion-title>{{ props.title }}</ion-title>
+      <ion-title @click="titleClicked">{{ props.title }}</ion-title>
       <ion-buttons slot="end" >
         <ion-toggle :checked="themeToggle" @ionChange="toggleChange($event)" justify="space-between"/>
       </ion-buttons>
@@ -36,38 +36,28 @@ import { chevronBack, contrast } from 'ionicons/icons';
 import { defineComponent, ref } from 'vue';
 
 const props = defineProps(['title', 'backButton'])
-
+const emits = defineEmits(['titleClicked'])
 const themeToggle = ref(false);
-
-// Use matchMedia to check the user preference
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-// Add or remove the "dark" class on the document body
 const toggleDarkTheme = (shouldAdd) => {
   document.body.classList.toggle('dark', shouldAdd);
 };
 
-// Check/uncheck the toggle and update the theme based on isDark
 const initializeDarkTheme = (isDark) => {
   themeToggle.value = isDark;
   toggleDarkTheme(isDark);
 };
 
-// Initialize the dark theme based on the initial
-// value of the prefers-color-scheme media query
 initializeDarkTheme(prefersDark.matches);
 
-// Listen for changes to the prefers-color-scheme media query
 prefersDark.addEventListener('change', (mediaQuery) => initializeDarkTheme(mediaQuery.matches));
 
-// Listen for the toggle check/uncheck to toggle the dark theme
 const toggleChange = (ev) => {
   toggleDarkTheme(ev.detail.checked);
 };
-</script>
 
-<style scoped>
-/* ion-toolbar {
-  --background: white;
-} */
-</style>
+function titleClicked () {
+  emits('titleClicked')
+}
+</script>
