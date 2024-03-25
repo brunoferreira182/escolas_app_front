d14619af211584c58ff80a1e00646973895e88db
Tiago Razera <tiago.razera@pelservicos.com.br>
2024-03-25 08:31:01 -0300

    versao

--- src/composables/variables.js
+++ src/composables/variables.js
@@ -1,8 +1,8 @@
 //////////// VARIAVEIS DA APLICACAO //////
-const MODE_MASTER_SERVER = 'local'
+const MODE_MASTER_SERVER = 'prod'
 const COMPANY_ID = 2
-const VERSION = 1.5
-const BUILD = 11
+const VERSION = 1.6
+const BUILD = 12
 //////////// NAO MUDAR //////
 const MODE_AUTH_SERVER = 'local'
 const MODE_APP_SERVER = 'local'
